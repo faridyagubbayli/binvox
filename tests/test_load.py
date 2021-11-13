c4abaa9748656f821161fcee08aea9fe74d3386a
Farid Yagubbayli <faridyagubbayli@gmail.com>
2021-11-13 23:20:42 +0100

    Added a new test case
    + Test case for the save functionality

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,14 +1,32 @@
 import numpy as np
-
 from tests import binvox_rw_original
 from binvox import Binvox
+import filecmp
 
 
 def test_loading():
-
+    # Load using original python script from Daniel Maturana
     with open('chair.binvox', 'rb') as f:
-        loaded_original = binvox_rw_original.read_as_3d_array(f)
+        loaded_old = binvox_rw_original.read_as_3d_array(f)
 
+    # Load with our library
     loaded_new = Binvox.read('chair.binvox', mode='dense')
 
-    assert np.allclose(loaded_original.data, loaded_new.numpy())
+    # Check for the correctness of the data
+    assert np.allclose(loaded_old.data, loaded_new.numpy())
+
+
+def test_saving():
+    # Load from binvox file
+    binvox = Binvox.read('chair.binvox', mode='dense')
+    # Save to binvox file
+    binvox.write('chair_new.binvox')
+
+    # Check for the equality of files
+    assert filecmp.cmp('chair.binvox', 'chair_new.binvox')
+
+    # Load from the newly saved file
+    binvox_duplicate = Binvox.read('chair_new.binvox', mode='dense')
+
+    # Check correctness of data
+    assert np.allclose(binvox.numpy(), binvox_duplicate.numpy())
